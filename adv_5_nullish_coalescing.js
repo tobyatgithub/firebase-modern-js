022d2efecc4a4c55d5104164ae5384ae63e89fa6
tobyatgithub <toby.fangyuan@gmail.com>
2023-07-16 21:46:08 -0700

    adv-5: Nullish Coalescing

---
+++ adv_5_nullish_coalescing.js
@@ -0,0 +1,9 @@
+// Nullish coalescing is a relativly new operator that was introduced in ES2020. 
+// It is similar to the logical OR operator ||, but it only returns the right-hand 
+// side if the left-hand side is null or undefined.
+
+const foo = null ?? "bar";
+console.log(foo); // bar
+
+const loo = 0 ?? "bar";
+console.log(loo); // 0
