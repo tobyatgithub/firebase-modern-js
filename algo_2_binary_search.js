f4423fc001ffad03337c970efc583ef1a4796db4
tobyatgithub <toby.fangyuan@gmail.com>
2023-07-20 16:36:50 -0700

    algo2: add binary search in js.

---
+++ algo_2_binary_search.js
@@ -0,0 +1,24 @@
+// PROBLEM: create a function that takes a sorted array
+// and a target value. Return the index of the target value
+// in the array. If the target value is not in the array,
+// return -1.
+
+const range = [...Array(100).keys()];
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  let mid = 0;
+  while (left <= right) {
+    mid = ((left + right) / 2) >> 0;
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) left = mid + 1;
+    if (arr[mid] > target) right = mid - 1;
+  }
+
+  return -1;
+}
+
+console.log(binarySearch(range, 2));
+console.log(binarySearch(range, 20));
+console.log(binarySearch(range, 200));
