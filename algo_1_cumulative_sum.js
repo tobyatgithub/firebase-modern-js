c66abd7fcc099f579d29faf2e7e23418bbe6b182
tobyatgithub <toby.fangyuan@gmail.com>
2023-07-20 16:29:53 -0700

    Algo1: add cumulative sum with 2 methods.

---
+++ algo_1_cumulative_sum.js
@@ -0,0 +1,18 @@
+// PROBLEM: create a function that takes an array of numbers and returns
+// a number that is the sum of all values in the array.
+
+const range = [...Array(100).keys()];
+
+// method 1. reduce()
+const result1 = range.reduce(
+  (accumulator, curVal) => (accumulator += curVal),
+  0
+);
+console.log(result1);
+
+// method 2. vanila for loop
+let result2 = 0;
+for (let i = 0; i < range.length; i++) {
+  result2 += range[i];
+}
+console.log(result2);
